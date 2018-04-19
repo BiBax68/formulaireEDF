24ec7c39f75b654eecb386bed72849caf1cc1cea
BiBax68 <lionel.farias@wanadoo.fr>
2018-04-19 20:01:49 +0200

    Add files via upload

---
+++ PPE4/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-J7KG7T8\SQLEXPRESS; Initial catalog=Formulaire; Integrated Security=True;");
+            try
+            {
+                if (sqlCon.State == System.Data.ConnectionState.Closed)
+                    sqlCon.Open();
+                string query = "SELECT COUNT (1) FROM Table_User WHERE Username=@Username AND Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = System.Data.CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username",txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    questionnaire_un dashboard = new questionnaire_un();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password is wrong");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+
+        }
+    }
+}
---
+++ PPE4/WpfApp1/WpfApp1/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour Window1.xaml
+    /// </summary>
+    public partial class questionnaire_un : Window
+    {
+        public questionnaire_un()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOui_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-J7KG7T8\SQLEXPRESS; Initial catalog=Formulaire; Integrated Security=True;");
+            SqlCommand sqlCommand = new SqlCommand("INSERT INTO reponse FROM Reponse VALUES ('" + BtnOui.Content + "')");
+            sqlCommand.ExecuteNonQuery();
+        
+        }
+
+        private void BtnNon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSansObjet_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnNecessiteCorrection_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
